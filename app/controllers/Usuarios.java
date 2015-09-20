7de6f4b8d071dece0c7387823765ecfa93b0f61d
EncarnaAmoros <encaramorosb@gmail.com>
2015-09-20 22:52:59 +0200

    Implementación de añadir nuevo usuario
    Se añade un nuevo usuario a la base de datos a través de un formulario

--- app/controllers/Usuarios.java
+++ app/controllers/Usuarios.java
@@ -2,14 +2,34 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
-
 import views.html.*;
+import models.*;
+import play.*;
+import play.mvc.*;
+import static play.libs.Json.*;
+import play.data.Form;
+import play.db.jpa.*;
 
 public class Usuarios extends Controller {
 
     public Result formularioNuevoUsuario() {
-      return ok(formularioNuevoUsuario.render());
-
+      return ok(formCreacionUsuario.render(Form.form(Usuario.class),""));
     }
+
+    @Transactional
+   // Añade un nuevo usuario en la BD y devuelve código HTTP
+   // de redirección a la página de listado de usuarios
+   public Result grabaNuevoUsuario() {
+     Form<Usuario> usuarioForm = Form.form(Usuario.class).bindFromRequest();
+     if (usuarioForm.hasErrors()) {
+       return badRequest(formCreacionUsuario.render(usuarioForm, "Hay errores en el formulario"));
+     }
+     Usuario usuario = usuarioForm.get();
+     usuario = UsuarioService.grabaUsuario(usuario);
+     flash("grabaUsuario", "El usuario se ha grabado correctamente");
+     return ok("Lista usuarios no creada aún");
+     //return redirect(controllers.routes.Usuarios.listaUsuarios());
+   }
+
 
 }
