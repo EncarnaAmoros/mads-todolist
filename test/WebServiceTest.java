116c3de257f55aafa49b508dbd8b1e201fa91d6c
EncarnaAmoros <encaramorosb@gmail.com>
2015-10-14 00:08:46 +0200

    Test de la capa Controlador testSaludo
    Modificacion automatica de IntegrationTest al ejecutar activator
    Empezamos con los test de la capa Controlador

---
+++ test/WebServiceTest.java
@@ -0,0 +1,23 @@
+import org.junit.*;
+
+import play.mvc.*;
+import play.test.*;
+import play.libs.F.*;
+
+import play.libs.ws.*;
+import play.Logger;
+
+import static play.test.Helpers.*;
+import static org.junit.Assert.*;
+
+public class WebServiceTest {
+    @Test
+    public void testSaludo() {
+        running(testServer(9000, fakeApplication(inMemoryDatabase())), () -> {
+            int timeout = 4000;
+            WSResponse response = WS.url("http://localhost:9000/saludo?nombre=Encarna").get().get(timeout);
+            assertEquals(OK, response.getStatus());
+            assertTrue(response.getBody().contains("Hola Encarna, bienvenido a"));
+        });
+    }
+}
