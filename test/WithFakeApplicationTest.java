b2f3f38e0a4bab0ced013b8d49bcdfb88b3e3d1d
EncarnaAmoros <encaramorosb@gmail.com>
2015-10-14 17:43:55 +0200

    Test testfindAllUsuariosDevuelveListaVacia
    Primer test de la capa de servicios y modelos

---
+++ test/WithFakeApplicationTest.java
@@ -0,0 +1,23 @@
+import org.junit.*;
+import play.test.*;
+import play.Application;
+import play.mvc.*;
+import static play.test.Helpers.*;
+import static org.junit.Assert.*;
+
+import play.db.jpa.*;
+import java.util.List;
+import models.*;
+
+public class WithFakeApplicationTest {
+
+    @Test
+    public void testfindAllUsuariosDevuelveListaVacia() {
+        running (fakeApplication(inMemoryDatabase()), () -> {
+            JPA.withTransaction(() -> {
+                List<Usuario> listaUsuarios = UsuarioService.findAllUsuarios();
+                assertTrue(listaUsuarios.isEmpty());
+            });
+        });
+    }
+}
