ac26236edab713c34cf8a253f57aacbff49cd197
EncarnaAmoros <encaramorosb@gmail.com>
2015-10-21 12:05:25 +0200

    Test testCreateTarea, DAO del model crea tarea

--- app/models/TareaDAO.java
+++ app/models/TareaDAO.java
@@ -12,4 +12,13 @@ public class TareaDAO {
     public static Tarea find(Integer idTarea) {
         return JPA.em().find(Tarea.class, idTarea);
     }
+
+    public static Tarea create (Tarea tarea) {
+      JPA.em().persist(tarea);
+      // Hacemos un flush y un refresh para asegurarnos de que se realiza
+      // la creación en la BD y se devuelve el id inicializado
+      JPA.em().flush();
+      JPA.em().refresh(tarea);
+      return tarea;
+    }
 }
